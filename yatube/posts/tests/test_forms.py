50ed280f7318e47fe0ade24c27636f83ee023a60
Юлиана Стрельникова <julianka.str@gmail.com>
2023-02-22 16:29:00 +0300

    final sprint

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -33,19 +33,7 @@ class PostCreateFormTests(TestCase):
             slug=GROUP_SLUG,
             description=GROUP_DESCRIPTION
         )
-        cls.small_gif = (
-            b'\x47\x49\x46\x38\x39\x61\x02\x00'
-            b'\x01\x00\x80\x00\x00\x00\x00\x00'
-            b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
-            b'\x00\x00\x00\x2C\x00\x00\x00\x00'
-            b'\x02\x00\x01\x00\x00\x02\x02\x0C'
-            b'\x0A\x00\x3B'
-        )
-        cls.uploaded = SimpleUploadedFile(
-            name='small.gif',
-            content=cls.small_gif,
-            content_type='image/gif'
-        )
+
         cls.the_post = Post.objects.create(
             author=cls.user,
             text=POST_TEXT,
@@ -59,12 +47,25 @@ class PostCreateFormTests(TestCase):
 
     def test_create_post(self):
         """Валидная форма создает пост с картинкой."""
+        small_gif = (
+            b'\x47\x49\x46\x38\x39\x61\x02\x00'
+            b'\x01\x00\x80\x00\x00\x00\x00\x00'
+            b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
+            b'\x00\x00\x00\x2C\x00\x00\x00\x00'
+            b'\x02\x00\x01\x00\x00\x02\x02\x0C'
+            b'\x0A\x00\x3B'
+        )
+        uploaded = SimpleUploadedFile(
+            name='small.gif',
+            content=small_gif,
+            content_type='image/gif'
+        )
         post_count = Post.objects.count()
         form_data = {
             'group': self.the_group.pk,
             'text': 'simple test',
             'author': self.user,
-            'image': self.uploaded,
+            'image': uploaded,
         }
         response = self.authorised_client.post(
             reverse('posts:post_create'),
@@ -84,6 +85,19 @@ class PostCreateFormTests(TestCase):
 
     def test_post_edit(self):
         """Валидная форма редактирования поста"""
+        small_gif1 = (
+            b'\x47\x49\x46\x38\x39\x61\x02\x00'
+            b'\x01\x00\x80\x00\x00\x00\x00\x00'
+            b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
+            b'\x00\x00\x00\x2C\x00\x00\x00\x00'
+            b'\x02\x00\x01\x00\x00\x02\x02\x0C'
+            b'\x0A\x00\x3B'
+        )
+        uploaded = SimpleUploadedFile(
+            name='smallest.gif',
+            content=small_gif1,
+            content_type='image/gif'
+        )
         another_group = Group.objects.create(
             title=GROUP_TITLE + ' 1',
             slug=GROUP_SLUG + '1',
@@ -93,7 +107,7 @@ class PostCreateFormTests(TestCase):
         form_edit_data = {
             'group': another_group.pk,
             'text': self.the_post.text + ' 1',
-            'image': self.uploaded
+            'image': uploaded
         }
         response = self.authorised_client.post(
             reverse('posts:post_edit',
@@ -102,14 +116,14 @@ class PostCreateFormTests(TestCase):
             follow=True
         )
         edit_post = Post.objects.get(pk=self.the_post.pk)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertEqual(Post.objects.count(), post_count)
+        self.assertRedirects(response, reverse('posts:post_detail',
+                             kwargs={'post_id': self.the_post.pk}))
         self.assertEqual(form_edit_data.get('text'), edit_post.text)
         self.assertEqual(another_group, edit_post.group)
         self.assertEqual(self.the_post.author, edit_post.author)
-        self.assertEqual('posts/small.gif', edit_post.image)
-        self.assertEqual(response.status_code, HTTPStatus.OK)
-        self.assertRedirects(response, reverse('posts:post_detail',
-                             kwargs={'post_id': self.the_post.pk}))
+        self.assertIsNotNone(edit_post.image)
 
 
 class CommentCreateFormTests(TestCase):
