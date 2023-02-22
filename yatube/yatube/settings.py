7ae3994f6fe7f26b16e7b1f37c77aa0e82788563
Юлиана Стрельникова <julianka.str@gmail.com>
2023-02-22 17:40:02 +0300

    final sprint

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -36,16 +36,16 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
-    'about.apps.AboutConfig',
-    'core.apps.CoreConfig',
-    'users.apps.UsersConfig',
-    'posts.apps.PostsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'about.apps.AboutConfig',
+    'core.apps.CoreConfig',
+    'users.apps.UsersConfig',
+    'posts.apps.PostsConfig',
     'sorl.thumbnail',
 ]
 
